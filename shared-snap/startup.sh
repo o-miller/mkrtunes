921b9876a6570a0bdb3af3841ec7232558829dbf
Owen <>
2025-03-01 15:09:24 -0500

    testing

--- shared-snap/startup.sh
+++ shared-snap/startup.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
+FIFO_DIR="/audio"
+CONFIG_FILE="/etc/snapserver.conf"
 
-/usr/bin/snapserver $ARGS
+echo "[stream]" > $CONFIG_FILE
+
+for fifo in ${FIFO_DIR}/*.fifo; do
+	[ -e "$fifo" ] || continue
+	NAME=$(basename "$fifo" .fifo)
+	echo "stream = pipe://$fifo?name=$NAME&codec=opus" >> $CONFIG_FILE
+done
+
+exec /usr/bin/snapserver -c $CONFIG_FILE
