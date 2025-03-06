83c2463074c0f9da18b5e4500f8ca9714cca6fde
owen <93544146+o-miller@users.noreply.github.com>
2025-03-06 11:34:49 -0500

    cleanin up

--- shared-snap/startup.sh
+++ shared-snap/startup.sh
@@ -7,7 +7,7 @@ echo "[stream]" > $CONFIG_FILE
 for fifo in ${FIFO_DIR}/*.fifo; do
 	[ -e "$fifo" ] || continue
 	NAME=$(basename "$fifo" .fifo)
-	echo "stream = pipe://$fifo?name=$NAME&codec=opus" >> $CONFIG_FILE
+	echo "stream = pipe://$fifo?name=$NAME&sampleformat=48000:24:2" >> $CONFIG_FILE
 done
 
 exec /usr/bin/snapserver -c $CONFIG_FILE
